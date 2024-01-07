308b98f0b3e89dcc58c231623da1d03be3147b20
Ajeet Singh <Ajeetsingh82900@gmail.com>
2024-01-07 11:41:02 +0530

    Card component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
-import Email from './components/Email';
+import Card from './components/Card';
 
 function App() {
-  return <center className='Email' >
-  <h1>Email</h1>
-  <Email></Email>
+  return <center >
+ 
+  <Card></Card>
+ 
     
   </center>
    
