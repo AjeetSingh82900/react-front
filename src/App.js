c4ae922692df34c4dfe7f91d0d4c8e5f61dd3028
Ajeet Singh <Ajeetsingh82900@gmail.com>
2024-01-07 11:03:01 +0530

    Checkbox component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-import Email from './components/Email';
+import Checkbox from './components/Checkbox';
 
 function App() {
-  return <center className='Email' >
-  <h1>Email</h1>
-  <Email></Email>
-    
+  return <center className='checkbox' >
+  <h1>Checkbox</h1>
+  
+    <Checkbox></Checkbox>
   </center>
    
     
